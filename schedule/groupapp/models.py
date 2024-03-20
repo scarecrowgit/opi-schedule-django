33b28e1ec23af34478fa8629b1b430e16afc8b81
SCARECROW <100871554+scarecrowgit@users.noreply.github.com>
2024-03-20 12:45:17 +0300

    feat: add new columns in table

--- schedule/groupapp/models.py
+++ schedule/groupapp/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class modelGroup(models.Model):
     chatId = models.CharField(max_length=100)
     groupId = models.CharField(max_length=100)
+    degree = models.CharField(max_length=20, choices=[('bachelor', 'Bachelor'), ('magister', 'Magister'),])
+    studyForm = models.CharField(max_length=20, choices=[('full-time', 'Full-time'), ('part-time', 'Part-time')])
+    course = models.IntegerField(default=1)
 
     def __str__(self):
         return self.chatId
